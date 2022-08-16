0a0d9418e6f7d1205eec3f3206be9df7bb82e753
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-16 12:01:41 -0700

    commit tree c76767c31da83c36b2b281ce4e82277e68badb19

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,12 @@
 class Network < ActiveRecord::Base
-  
+
+    has_many :shows
+
+    # def self.shows
+    #    self.show.all
+    # end
+
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
 end
